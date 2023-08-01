81c5a1e87e8b0d2333235f95f84725d65f67ba11
SDE <sieg2011@colorado.edu>
2023-08-01 21:40:32 +0000

    included make_roomID function in plugin.py

--- plugin.py
+++ plugin.py
@@ -3,12 +3,19 @@
 ############################
 # hold additional functions 
 
+from dbconnection import create_connection
+from flask import Flask
+import psycopg2
+import os
+from dotenv import load_dotenv
 
 
-
-def MakeRoomID(HostName):
+def make_roomID(HostName):
     '''take the entered hostname as input and construct short
     noncryptographic hash code to be used to find room'''
-    
-    
-    return HostName
+        roomID = 33
+    if len(HostName) < 5:
+        HostName *= 3
+    for char in HostName:
+        roomID = ((( roomID << 5) + roomID) + ord(char)) & 0xFFFFF
+    return str(hex(roomID))[2:]
