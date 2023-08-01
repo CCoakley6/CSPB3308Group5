53b3ae3673c4cf762af1e535a12983b6ea58ace9
SDE <sieg2011@colorado.edu>
2023-08-01 22:34:42 +0000

    included make_GameHistory function in plugin.py

--- plugin.py
+++ plugin.py
@@ -10,12 +10,31 @@ import os
 from dotenv import load_dotenv
 
 
-def make_roomID(HostName):
+def make_RoomID(HostName):
     '''take the entered hostname as input and construct short
     noncryptographic hash code to be used to find room'''
-        roomID = 33
+        RoomID = 33
     if len(HostName) < 5:
         HostName *= 3
     for char in HostName:
-        roomID = ((( roomID << 5) + roomID) + ord(char)) & 0xFFFFF
-    return str(hex(roomID))[2:]
+        RoomID = ((( RoomID << 5) + RoomID) + ord(char)) & 0xFFFFF
+    return str(hex(RoomID))[2:]
+
+
+def make_GameHistory(RoomID, Winner):
+    '''generate and insert a new record into GameHistory table. 
+    RoomID and Winner are strings, and Winner must be identical 
+    to a field of PlayerRosters table, as in 'Player3'.'''
+    # Use the database connection function from dbconnection.py
+    connection = create_connection()
+    cur = connection.cursor()
+    cur.execute('''
+                INSERT INTO GameHistory (RoomID, RoomTitle, HostName, Players, Winner)
+                SELECT ActiveSessions.*, PlayerRosters.''' +Winner+ '''
+                FROM ActiveSessions
+                LEFT JOIN PlayerRosters
+                ON ActiveSessions.RoomID=PlayerRosters.RoomID
+                WHERE RoomID=''' +RoomID+ '''
+                ''')
+    connection.commit()
+    connection.close()
