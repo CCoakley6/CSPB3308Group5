e05223331858ff385ee3cbd3f9016dba6dcc3777
SDE <sieg2011@colorado.edu>
2023-08-01 20:48:06 +0000

    added plugin.py to main directory, use for additional functionality

---
+++ plugin.py
@@ -0,0 +1,14 @@
+# plugin.py
+# import this into flask app
+############################
+# hold additional functions 
+
+
+
+
+def MakeRoomID(HostName):
+    '''take the entered hostname as input and construct short
+    noncryptographic hash code to be used to find room'''
+    
+    
+    return HostName
